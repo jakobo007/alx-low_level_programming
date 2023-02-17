c4257b6456df0a40775e2ec4da0e6aca72a38eed
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-17 10:05:59 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@ function
 */
 int main(void)
 {
-puts("/ \"Programming is like building a multilingual puzzle\n"");
+puts("/ \"Programming is like building a multilingual puzzle\"");
 
 return(0);
 }
