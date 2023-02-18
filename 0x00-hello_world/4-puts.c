f1def939e712b896ba27b5885e388e93d855b4f3
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-18 21:18:23 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
 *main - A program that prints a line with puts
 function
-*Return: 0 (Success)
+*Return: 0 ( Success )
 */
 int main (void)
 {
-puts("\"Programming is like building a multilingual puzzle\"");
+puts ("\"Programming is like building a multilingual puzzle\"");
 
 return(0);
 }
