d93a77bfde0b31d5cf373e14fa4edc2b34b69a21
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-19 16:29:41 +0300

    Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*betty style doc for function main goes there */
+/**
+*main - program that prints all the numbers of base 16 in lowercase, followed by a new line
+*Return: always 0
+*/
+int main() {
+    int i;
+    for (i = 0; i < 10; i++) {
+        putchar('0' + i);
+    }
+    for (i = 0; i < 6; i++) {
+        putchar('a' + i);
+    }
+    putchar('\n');
+    return 0;
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*betty style doc for function main goes there */
+/**
+*main - program that prints all possible combinations of single-digit numbers
+*Return: always 0
+*/
+int main() {
+    int i, j;
+
+    for (i = 0; i < 10; i++) {
+        for (j = i; j < 10; j++) {
+            putchar(i + '0');
+            putchar(j + '0');
+            if (i != 9 || j != 9) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
+}
