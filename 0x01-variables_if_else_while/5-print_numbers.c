71f89ed42c843de200fdc8d621a9be58c0d674a3
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-19 16:39:12 +0300

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,11 @@
 */
 int main()
 {
-  int i;
-    for (i = 0; i <= 9; i++) {
-        putchar(i + '0');
-    }
-    putchar('\n');
-    return 0;  
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return 0;  
 }
