f60a8c9ded0346ce85e409c7bbb925f00394b2d0
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-23 01:31:23 +0300

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * time_table - multiplication of 9
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+int i;
+for (i = 0; i <= 10; i++) {
+printf("%d x 9 = %d\n", i, i*9);]
+}
+return i;
+}
