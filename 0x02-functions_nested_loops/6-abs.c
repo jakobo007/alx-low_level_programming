46909aac955f3b997717182cb75f6f06735f1e2d
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-23 00:28:14 +0300

    no such thing as absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - check the code for absolute value
+ *
+ * @a parameter to be checked
+ * 
+ * Return: Always a.
+ */
+int _abs(int)
+{
+if ( a < 0)
+a = -(a);
+else if (a >= 0)
+a = a;
+}
