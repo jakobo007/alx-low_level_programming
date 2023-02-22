2db42bb329cb32f684bbd09edab2b94ee434d5d3
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-22 21:09:50 +0300

    the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char i;
 for (i = 'a'; i <= 'z')
