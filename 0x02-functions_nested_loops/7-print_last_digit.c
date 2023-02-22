04554e0851f4de369003c1bb17199de4012e9827
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-23 00:46:06 +0300

    3 colors, 10 digits, and 7 notes

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _last_digit - prints the last digit of a number
+ *
+ * @x: function parameter
+ * 
+ * Return: y.
+ */
+int print_last_digit(int x)
+{
+int y;
+y = x % 10;
+if (x < 0)
+y = -y;
+return (y);
+}
