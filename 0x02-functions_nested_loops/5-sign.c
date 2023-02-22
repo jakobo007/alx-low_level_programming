460cb8c508c45186db32276116f3c0bf3732c827
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-22 22:53:01 +0300

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,9 +9,24 @@
 int print_sign(int n)
 {
 if (n > 0)
-return (1);
-else if (n = 0)
+{
+_putchar('+');
+_putchar(',');
+_putchar(' ');
+return (+);
+}
+else if (n == 0)
+{
+_putchar('+');
+_putchar(',');
+_putchar(' ');
 return (0);
-else  if (n < 0)
+}
+else
+{
+_putchar('+');
+_putchar(',');
+_putchar(' ');
 return (-1);
+}
 }
