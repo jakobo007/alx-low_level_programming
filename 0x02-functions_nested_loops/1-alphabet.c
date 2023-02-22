9e70963d50880c28bbc4b7b075d0b9df3167a689
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-22 21:07:19 +0300

    the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _putchar - prints the alphabet, in lowercase
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void);
+{
+char i;
+for (i = 'a'; i <= 'z')
+putchar(i)
+putchar('\n')
+}
