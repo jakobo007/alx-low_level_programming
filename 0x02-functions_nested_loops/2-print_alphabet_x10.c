a03e7b3c6290e4c13de416b399e51b0600923651
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-22 22:01:56 +0300

    10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*betty style doc for function main goes there */
 /**
-*main - 10x alphabet
+*main - 10x void print_alphabet_x10(void)
 *Return: always (0)
 */
 void print_alphabet_x10(void)
@@ -9,11 +9,11 @@ void print_alphabet_x10(void)
 char c;
 int i;
 for (i = 1; i <= 10; i++)
-{      
+{    
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
-}  
+}
 _putchar('\n');
 }
 }
