b1ad6f113b71c6d419965f2649a14124c5990954
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-23 02:19:20 +0300

    gcc

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,11 +11,11 @@ int main(void)
     sum = 0;
     for ( i = 3; 1 < 10; i++)
     {
-        if (i % 3 == 0) || (i % 5 == 0)
+        if ((i % 3 == 0) || (i % 5 == 0))
         {
             sum = sum + i;
         }
     }
-    printf("&d\n",sum);
+    printf("%d\n",sum);
     return(0);
 }
