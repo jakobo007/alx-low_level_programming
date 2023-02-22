7edb20424fab464b4cb0ce206ff2502f43b2acf7
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-22 14:46:27 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*betty style doc for function main goes there */
+/**
+*main - Print alphabets
+*Return: always (0)
+*/
+int main(void)
+{
+    printf(_putchar);
+    return (0);
+}
