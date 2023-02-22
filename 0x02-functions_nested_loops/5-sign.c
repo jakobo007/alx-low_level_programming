7e417b5b58afea89fc256761b113f6c5edb00531
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-22 22:44:49 +0300

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_sign - function that prints lowercase
+ *
+ * @c: parameter to be printed
+ *
+ * Return: 1 and prints + if n is greater than zero 
+ */
+int print_sign(int n)
+{
+if (n > 0)
+return (1);
+else if (n = 0)
+return (0);
+else  if (n < 0)
+return (-1);
+}
