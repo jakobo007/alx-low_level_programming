127873968ec92ad794e2e0f6b8f67ed6e093681a
jakobo007 <oumajamesjoshua@gmail.com>
2023-02-25 00:39:48 +0300

    You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function declaration
+ * Description: print a diagonal line
+ * @n: number of parts
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int slash;
+	int space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (slash = 0; slash < n; slash++)
+		{
+			for (space = 0; space < slash; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - function declaration
+ * Description: prints a square  of hash symbols
+ * @size: the size of the square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int row;
+	int column;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < size; column++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
