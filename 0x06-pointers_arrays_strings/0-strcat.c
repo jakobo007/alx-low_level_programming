0871eb8193f5bc648fdcd639373aeff5de916b4e
jakobo007 <oumajamesjoshua@gmail.com>
2023-03-01 11:38:57 +0300

    More pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,2 +1,23 @@
 #include "main.h"
-
+/**
+ * *_strcat - check the code
+ * @dest: pointer string
+ * @src: pointer
+ * @p: variable
+ * Return: Void
+ */
+char *_strcat(char *dest, char *src) {
+    char *p = dest;
+    
+    while (*p != '\0') {
+        p++;
+    }
+    
+    while (*src != '\0') {
+        *p++ = *src++;
+    }
+    
+    *p = '\0';
+    
+    return dest;
+}
