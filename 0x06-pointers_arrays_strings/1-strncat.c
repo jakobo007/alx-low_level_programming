e1964c96b0f25398c931664d5256511635f19868
jakobo007 <oumajamesjoshua@gmail.com>
2023-03-01 13:53:32 +0300

    pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,8 +10,6 @@ char *_strncat(char *dest, char *src, int n)
 {
 char dest[50] = "Hello, ";
 char src[] = "world!";
-
-_strncat(dest, src, 5);  // concatenate at most 5 characters from src to dest
-
-printf("%s\n", dest);   // prints "Hello, world"
+_strncat(dest, src, 5);
+printf("%s\n", dest);
 }
