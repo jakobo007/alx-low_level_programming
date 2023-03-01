60fd596ffd9841eaa8fb47ed139a9868b881fafe
jakobo007 <oumajamesjoshua@gmail.com>
2023-03-01 14:04:31 +0300

    katonje

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,17 +6,18 @@
  *@n: variable
  * Return: Void
 */
-char *_strncat(char *dest, char *src, int n) {
-    char *result = dest;  
-        while (*dest != '\0')
-    {
-        dest++;
-    }
-    while (n > 0 && *src != '\0')
-    {
-        *dest++ = *src++;
-        n--;
-    }
-    *dest = '\0';
-    return (result);
+char *_strncat(char *dest, char *src, int n)
+{
+char *result = dest;  
+while (*dest != '\0')
+{
+dest++;
+}
+while (n > 0 && *src != '\0')
+{
+*dest++ = *src++;
+n--;
+}
+*dest = '\0';
+return (result);
 }
