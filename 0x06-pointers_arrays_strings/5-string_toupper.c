f9ae98072ee4941c3dcaf441e8dec7a77fab4f77
jakobo007 <oumajamesjoshua@gmail.com>
2023-03-02 10:00:35 +0300

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *string_toupper - function
+ *@str: pointer string
+ *@ptr: string
+ * Return: Void
+*/
+char *string_toupper(char *str)
+{
+char *ptr = str;
+while (*ptr != '\0')
+{
+if (islower(*ptr)) {
+*ptr = toupper(*ptr);
+}
+ptr++;
+}
+return (str);
+}
