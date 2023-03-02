94e2ccd6cd3329f7db92f8a366262cc3126957cd
jakobo007 <oumajamesjoshua@gmail.com>
2023-03-02 10:06:52 +0300

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,14 +3,19 @@
  *string_toupper - function
  *@str: pointer string
  *@ptr: string
+ *@islower: variable
+ *@toupper: variable
  * Return: Void
 */
 char *string_toupper(char *str)
 {
+int islower;
+int toupper;
 char *ptr = str;
 while (*ptr != '\0')
 {
-if (islower(*ptr)) {
+if (islower(*ptr))
+{
 *ptr = toupper(*ptr);
 }
 ptr++;
