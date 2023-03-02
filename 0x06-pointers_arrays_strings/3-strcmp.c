4b094908d70d3a075a762412b7a4c142158616a7
jakobo007 <oumajamesjoshua@gmail.com>
2023-03-02 09:40:56 +0300

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strcmp - function
+ *@s1: pointer string
+ *@s2: string
+ * Return: Void
+*/
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+return (*s1 == *s2) ? 0 : (*s1 < *s2) ? -1 : 1;
+}
