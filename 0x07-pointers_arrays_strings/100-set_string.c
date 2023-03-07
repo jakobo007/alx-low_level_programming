c238328a9a468ed5a526cfd5643723ffe659bf31
jakobo007 <oumajamesjoshua@gmail.com>
2023-03-08 02:15:52 +0300

    intellectual curiosity

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - function declaration
+ * @s: the pointer
+ * @to: the char
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function declaration
+ * @a: pointer array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[row][column]);
+		}
+		_putchar('\n');
+	}
+}
