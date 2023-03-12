10450ae906f46ef0bad0900594b5dbf84515786a
jakobo007 <oumajamesjoshua@gmail.com>
2023-03-12 03:17:54 +0300

    all .c files

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - function declaration
+ * Description: find length of a string
+ * @s: the string ptr
+ * Return: index
+ */
+
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{}
+	return (index);
+}
