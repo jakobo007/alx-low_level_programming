4b2e06f5ac08d2a96df580802d63ba7bbd81b946
jakobo007 <oumajamesjoshua@gmail.com>
2023-09-27 19:53:34 +0300

    Length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function declaration
+ * @s: string value
+ * Return: string length
+*/
+
+int main(void)
+{
+    _strlen_recursion("Corbin Coleman")
+}
+
+int _strlen_recursion(char *s)
+{
+    int lenght = 0;
+    if (*s)
+    {
+        lenght = _strlen_recursion(s + 1);
+        return (lenght = lenght + 1);
+    }
+    return (0);
+}
