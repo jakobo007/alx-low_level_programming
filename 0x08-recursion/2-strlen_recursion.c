79b6984a517a4f3b27cc2cc1b6dd86ed1fa97223
jakobo007 <oumajamesjoshua@gmail.com>
2023-09-28 08:21:52 +0300

    String length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,33 +1,17 @@
 #include "main.h"
 /**
  * _strlen_recursion - function declaration
+ * 
  * @s: string
+ * 
  * Return: string length
 */
 
 int _strlen_recursion(char *s);
-
-int main(void)
 {
-    _strlen_recursion("Corbin Coleman");
-}
-
-/**
- * _strlen_recursion - function definition
- * @s: the string to measure
- * Description: finds the length of a string
- * using recursion
- * Return: the string length
- */
-
-
-int _strlen_recursion(char *s)
-{
-    int length = 0;
-    if (*s)
+    if (*s != 0)
     {
-        length = _strlen_recursion(s + 1);
-        return (length = length + 1);
+        return(1 + _strlen_recursion(s + 1));
     }
-    return (0);
+    return  (0);
 }
