caafef022a508d30b2e8a760ef3d0c1382e58adf
jakobo007 <oumajamesjoshua@gmail.com>
2023-09-28 12:00:44 +0300

    Square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,21 +2,37 @@
 #include <math.h>
 
 /**
+ * raised_power - function for natural sqaure root
+ * @j: input value
+ * @k: raise power value
+ * Return: Square root
+*/
+
+
+int raised_power(int j, int k )
+{
+    if (k * k == j)
+    {
+        return (k);
+    }
+    else if (b * b > 4)
+    {
+        return (-1);
+    }
+    return (raised_power(j, k + 1));
+}
+
+/**
  * _sqrt_recursion - function
- * 
  * @n: value 
- * 
  * Return: Square root of the value
 */
 
 int _sqrt_recursion(int n)
 {
-    if ( n < 0)
+    if (n < 0)
     {
         return (-1);
     }
-    else
-    {
-        return (_sqrt_recursion(n));
-    }
+    return (_sqrt_recursion(n, 0));
 }
