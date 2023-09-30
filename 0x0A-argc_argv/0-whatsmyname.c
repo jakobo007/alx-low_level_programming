c62b54d7df149ec2d53b213b1f404a00b182dd1b
jakobo007 <oumajamesjoshua@gmail.com>
2023-09-30 17:05:02 +0300

    Slight chabges

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: string 
  * Return: (0)
  */
-int main (int argc  __attribute__((unused)), char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
     printf("%s\n", argv[0]);
     return (0);
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- to print name
+ * @argc: arguments
+ * @argv: string
+ * @i: integer
+ * Return: (0)
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+    int i;
+    for (i = 0; i < argc; i++)
+    {
+        printf("argv[%d] = %s\n", i, argv[i] );
+    }
+    return 0;
+}
