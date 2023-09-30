af347b8fd61ef62f69585809bdcb07a1c86b89a4
jakobo007 <oumajamesjoshua@gmail.com>
2023-09-30 17:35:40 +0300

    Trial

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- to print name
+ * @argc: arguments
+ * @argv: string
+ * @i: integer
+ * Return: (0)
+ */
+int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
+{
+    int x;
+    int y;
+    int times;
+    times = x * y;
+    if (argc > 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+}
