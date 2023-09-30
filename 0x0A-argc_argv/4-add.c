0fd0215d91ceab52d94f3142ae152333326f0c99
jakobo007 <oumajamesjoshua@gmail.com>
2023-09-30 18:01:17 +0300

    To beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,7 +8,7 @@
  */
 int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 {
-    if (argc != 3)
+    if (argc != 3, argc = 0)
     {
         printf("Error\n");
         return (1);
