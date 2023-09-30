23eed7b8238906c9d9d43529942af3aef794ff12
jakobo007 <oumajamesjoshua@gmail.com>
2023-09-30 16:50:15 +0300

    slightc hange

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+/**
+ * main- to print name
+ * @argc: arguments
+ * @argv: string 
 
+ * Return: (0)
+ */
 int main (int argc  __attribute__((unused)), char *argv[])
 {
     printf("%s\n", argv[0]);
     return (0);
 }
+
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - function to call
+ * @argc: arguments
+ * @argv: array of string values
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc);
+	return 0;
+}
