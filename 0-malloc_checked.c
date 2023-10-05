8e4f5aa0f8945eb69b1bb276bc52b75bd899797e
jakobo007 <oumajamesjoshua@gmail.com>
2023-10-05 07:33:37 -0700

    intial commit

---
+++ 0-malloc_checked.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - function prototype
+ * @b: input value
+ * Return: Pointer on sucess
+ * /
+void *malloc_checked(unsigned int b)
+{
+
+}
+
