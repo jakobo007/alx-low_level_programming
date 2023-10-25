8c83545ad978bd40707bc89c35274d2244f34f43
jakobo007 <oumajamesjoshua@gmail.com>
2023-10-25 20:47:51 +0300

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * root - square root
+ * @y: input value
+ * @z: raise value
+ *Return: sqaure root
+*/
+int root(int y, int z)
+{
+if (z * z == y)
+{
+return (y);
+}
+else if (z * z > y)
+{
+return (-1);
+}
+return (root(z, y + 1));
+}
+/**
+* _sqrt_recursion - return square root
+* @n: input
+* Return: sqaure root
+*/
+int _sqrt_recursion(int n)
+{
+return (root(n, 0));
+}
