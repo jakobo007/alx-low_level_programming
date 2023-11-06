e8b3e1ac1d88403bcd046ecf239b0bbeb0a425de
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-06 20:33:20 +0300

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#define DOG_H
+/**
+ * struct dog - dog details
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * 
+ * Return
+*/
+
+struct dog {
+    char *name;
+    char *owner;
+    float age;
+};
