52c9322a36f92b98462e063d738843af81fbf972
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-06 20:41:11 +0300

    only thing on earth that loves you

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "doh.h"
+/**
+ * init_dog - function
+ * @dog: dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * 
+ * Return
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner) {
+    if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,3 +13,7 @@ struct dog {
     char *owner;
     float age;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
