0cdddf15444e1ffd70ebd062fafac346896fc89f
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-06 21:00:28 +0300

    a book is a man's best friend

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,7 +3,7 @@
 /**
  * print_dog - function
  * @d: pointer to struct
- * Return 
+ * Return
 */
 void print_dog(struct dog *d)
 {
@@ -13,5 +13,5 @@ if (d->name == NULL)
 d->name = "(nil)";
 if (d->owner == NULL)
 d->owner = "(nil)";
-printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
