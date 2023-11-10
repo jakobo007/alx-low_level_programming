3ded1a187533cdefd75cd1f42e7e5f78b5bcf80c
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-10 05:56:24 +0300

    Ugliness is a constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i, sum = 0;
-	va_start(args, n);
 
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args,int);
+		sum += va_arg(args, int);
 	}
 	va_end(args);
-	return sum;
+	return (sum);
 }
 
