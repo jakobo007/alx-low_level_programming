9e360ab1b7aaa8db196c33982fc067031842a395
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-16 15:23:03 +0300

    List Length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,11 +3,11 @@
 #include <string.h>
 #include "lists.h"
 /**
- * print_list - function to print elements of a linked list
+ * list_len - function to print elements of a linked list
  * @h: node head
  * Return: Elements in the list
 */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
     size_t counter = 0;
     while(h != NULL)
