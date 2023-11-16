36779c6e9e72dc6b1375cab0154d0400212495a5
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-16 15:46:46 +0300

    Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * 
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *new_node = (list_t *)malloc(sizeof(list_t));
+    if (new_node == NULL)
+    return NULL;
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    return NULL;
+
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
+    return (new_node);
+}
