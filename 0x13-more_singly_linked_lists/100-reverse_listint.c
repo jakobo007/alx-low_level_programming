792cf82d06eef9b2448fb80db0e4fd1e7250fc1f
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-22 17:51:06 +0300

    Advance++

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,18 +8,16 @@
 */
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *prev = NULL;
-    listint_t *current = *head;
-
-    while (current != NULL)
-    {
-        listint_t *next_node;
-        next_node = current->next;
-        current->next = prev;
-        prev = current;
-        current =  next_node;
-    }
-
-    *head = prev;
-    return (*head);
+listint_t *prev = NULL;
+listint_t *current = *head;
+while (current != NULL)
+{
+listint_t *next_node;
+next_node = current->next;
+current->next = prev;
+prev = current;
+current =  next_node;
+}
+*head = prev;
+return (*head);
 }
