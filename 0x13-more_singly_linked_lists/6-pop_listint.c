a30570f434ddc2604dc2ca2ab7506222e15730a5
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-22 06:08:49 +0300

    pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,9 +9,11 @@
 */
 int pop_listint(listint_t **head)
 {
+int data;
+listint_t *temp;
 if (*head == NULL)
 return (0);
-int data = (*head)->n;
+data = (*head)->n;
 listint_t *temp =*head;
 *head = (*head)->next;
 free(temp);
