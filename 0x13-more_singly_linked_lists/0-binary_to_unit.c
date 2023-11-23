fe82f1a36841dfabfcda6742260e4fca37330253
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-23 14:08:11 +0300

    Bit manipulation

---
+++ 0x13-more_singly_linked_lists/0-binary_to_unit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * binary_to_unit -  function to convert binary to unsigned 
+ * @b: A pointer to a string of 0 and 1
+ * Return: The converted number
+*/
+unsigned int binary_to_uint(const char *b)
+{
+
+    unsigned int result = 0;
+    if (b == NULL)
+    return (0);
+    while (*b)
+    {
+        if (*b == '0' || *b == '1')
+        {
+            result =result *2 + (*b - '0');
+        }
+        else 
+        {
+            return (0);
+        }
+        b++;
+    }
+return (result);
+}
