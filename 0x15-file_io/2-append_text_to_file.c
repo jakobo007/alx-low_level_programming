af6c9a1a166dffc8f48db960d8031533336f0916
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-28 16:08:02 +0300

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,11 @@
+#include <string.h>
+#include <sys/types.h>
+#include "main.h"
 #include <fcntl.h>
+#include <stddef.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <sys/stat.h>
 int append_text_to_file(const char *filename, char *text_content);
 
 /**
@@ -17,11 +21,13 @@ int append_text_to_file(const char *filename, char *text_content) {
     if (filename == NULL)
         return -1;
 
+    // Open the file in append mode, if it doesn't exist return -1
     fd = open(filename, O_WRONLY | O_APPEND);
     if (fd == -1)
         return -1;
 
     if (text_content != NULL) {
+        // Write the content to the file
         write_status = write(fd, text_content, _strlen(text_content));
         if (write_status == -1) {
             close(fd);
