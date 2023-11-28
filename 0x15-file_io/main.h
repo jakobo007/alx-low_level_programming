e15f6d0402b2b5f92e475ce913b5ff8500a1d309
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-28 15:47:01 +0300

    under sow

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,6 @@
 #define _MAIN_H_
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 #endif
