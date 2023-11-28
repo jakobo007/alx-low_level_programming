e80cff285c4c8279e05a8980bb35f9885336eebd
jakobo007 <oumajamesjoshua@gmail.com>
2023-11-28 16:11:56 +0300

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,14 +21,13 @@ int append_text_to_file(const char *filename, char *text_content) {
     if (filename == NULL)
         return -1;
 
-    // Open the file in append mode, if it doesn't exist return -1
     fd = open(filename, O_WRONLY | O_APPEND);
     if (fd == -1)
         return -1;
 
     if (text_content != NULL) {
         // Write the content to the file
-        write_status = write(fd, text_content, _strlen(text_content));
+        write_status = write(fd, text_content, strlen(text_content));
         if (write_status == -1) {
             close(fd);
             return -1;
