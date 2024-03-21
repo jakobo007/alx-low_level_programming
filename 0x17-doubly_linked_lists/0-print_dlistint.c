9a80b3eb790c1477bb79dda2628ebec4b08bd60a
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-03-21 13:26:49 +0300

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "lists.h"
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
     size_t counter = 0;
     while (h != NULL) {
