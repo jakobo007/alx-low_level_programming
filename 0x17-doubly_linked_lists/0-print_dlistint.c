07db72a0f43bb2f3fca4c1a73234a64d8eb884dd
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-03-21 13:41:49 +0300

    print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,15 +5,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t counter = 0;
-    while (h != NULL) {
-        if (h->str == NULL)
-        {
-            printf("[%u] %s\n", h->len, h->str);
-        }
-
-        h = h->next;
-        counter++;
-    }
-    return (counter);
+    size_t count = 0;
+    const dlistint_t *current = h;
+     while (current != NULL)
+     {
+        printf("%d\n", current->n);
+        current = current->next;
+        count++;
+     }
+     
+     return count;
 }
