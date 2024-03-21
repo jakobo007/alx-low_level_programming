45b7032a71ca2d7ede0176fc5b63616462e572e5
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-03-21 16:23:30 +0300

    add node at end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -5,24 +5,35 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
+   dlistint_t *new_node;
+    dlistint_t *last_node = *head;
 
+    /* Allocate memory for the new node */
     new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-    {
+    if (new_node == NULL) {
+        /* Allocation failed, return NULL */
         return NULL;
     }
 
+    /* Set the value of the new node */
     new_node->n = n;
-    new_node->prev = *head;
     new_node->next = NULL;
 
-    if (*head != NULL) {
-        (*head)->prev = new_node;
+    /* If the list is empty, make the new node the head */
+    if (*head == NULL) {
+        new_node->prev = NULL;
+        *head = new_node;
+        return new_node;
     }
 
-    /* Update head pointer to point to the new node */
-    *head = new_node;
+    /* Traverse the list to find the last node */
+    while (last_node->next != NULL) {
+        last_node = last_node->next;
+    }
+
+    /* Link the new node to the last node */
+    last_node->next = new_node;
+    new_node->prev = last_node;
 
     return new_node;
 }
