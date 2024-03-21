642fb33badc6204b1a37b475c75a02acc4040996
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-03-21 16:04:23 +0300

    Add node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    dlistint_t *new_node;
+
+    /* Allocate memory for the new node */
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL) {
+        /* Allocation failed, return NULL */
+        return NULL;
+    }
+
+    /* Set the value of the new node */
+    new_node->n = n;
+
+    /* Update pointers to link the new node */
+    new_node->prev = NULL;
+    new_node->next = *head;
+
+    /* Update head pointer if the list is not empty */
+    if (*head != NULL) {
+        (*head)->prev = new_node;
+    }
+
+    /* Update head pointer to point to the new node */
+    *head = new_node;
+
+    return new_node;
+}
