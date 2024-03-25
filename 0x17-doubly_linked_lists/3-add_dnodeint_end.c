113f5a41029b58e1793be3654156d379266d9197
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-03-25 22:39:21 +0300

    Betty changes

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,8 @@ dlistint_t *new_node;
 dlistint_t *last_node = *head;
 /* Allocate memory for the new node */
 new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL) {
+if (new_node == NULL)
+{
 /* Allocation failed, return NULL */
 return (NULL);
 }
@@ -22,13 +23,15 @@ return (NULL);
 new_node->n = n;
 new_node->next = NULL;
 /* If the list is empty, make the new node the head */
-if (*head == NULL) {
+if (*head == NULL)
+{
 new_node->prev = NULL;
 *head = new_node;
 return (new_node);
 }
 /* Traverse the list to find the last node */
-while (last_node->next != NULL) {
+while (last_node->next != NULL)
+{
 last_node = last_node->next;
 }
 /* Link the new node to the last node */
