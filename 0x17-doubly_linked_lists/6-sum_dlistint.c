e577c2cca9ce82a715159c6eea1bc07a0b6594d1
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-03-25 11:50:46 +0300

    Sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int sum_dlistint(dlistint_t *head)
+{
+    int sum = 0;
+    dlistint_t *current = head;
+    while(current != NULL)
+    {
+        sum += current->n;
+        current = current->next;
+    }
+
+    return sum;
+}
