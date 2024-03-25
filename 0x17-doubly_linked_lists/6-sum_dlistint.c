3efa988a0a7cc914f5a3139998d8050a5b952591
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-03-25 22:49:22 +0300

    betty changes

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,9 +4,9 @@
 #include "lists.h"
 /**
  * sum_dlistint - calculate the sum of all data
- * @head: pointer to head of list 
- * Return: Sum of the data(n) 
- */ 
+ * @head: pointer to head of list
+ * Return: Sum of the data(n)
+ */
 int sum_dlistint(dlistint_t *head)
 {
 int sum = 0;
