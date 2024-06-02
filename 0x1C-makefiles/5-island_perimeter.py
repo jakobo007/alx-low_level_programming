a144569784ec3bbd8efeb5db6d486f2c7e3f102f
jakobo007 <oumajames>
2024-06-02 11:10:05 -0700

    Island Perimeter_1

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,13 +4,13 @@ Function that returns the perimeter of the island described in grid
 """
 
 def island_perimeter(grid):
-    """Grid is a nested list
-    0 represents a water zone
-    1 represents a land zone
-    one cell is a square with side length 1
-    Grid cells are conncted horizontally/vertically (not diagonally)
-    grid is rectangular, width and height don't exceed 100
-    """
+    """Grid is a nested list"""
+    """0 represents a water zone"""
+    """1 represents a land zone"""
+    """one cell is a square with side length 1"""
+    """Grid cells are conncted horizontally/vertically (not diagonally)"""
+    """grid is rectangular, width and height don't exceed 100"""
+
     width = len(grid[0])
     height = len(grid)
     edges = 0
