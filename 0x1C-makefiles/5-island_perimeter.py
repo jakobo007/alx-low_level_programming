ff7227ed5cfb27de1175ea8dce0e338fe65a6efb
jakobo007 <oumajames>
2024-06-02 11:13:19 -0700

    Island Perimeter_3

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Function that returns the perimeter of the island described in grid
-"""
+"""Function that returns the perimeter of the island described in grid"""
 
 
 def island_perimeter(grid):
