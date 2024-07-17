fc94fa9d80bcfbbc56f0f6813eefb654bf81ab85
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-07-17 18:33:17 +0300

    Binary 3

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,23 +4,24 @@
  * @array: array to loop
  * @size: size of the array
  * @value: search value
- * Return value
+ * Return: value
  * **/
 int linear_search(int *array, size_t size, int value)
 {
-    size_t index;
-    if (array == NULL)
-    {
-        return (-1);
-    }
+size_t index;
 
-    for (index = 0; index < size; index++)
-    {
-        printf("Value checked array[%u] = [%d]\n", (unsigned int)index, array[index]);
-        if (array[index] == value)
-        {
-            return (index);
-        }
-    }
-    return (-1);
+if (array == NULL)
+{
+return (-1);
+}
+
+for (index = 0; index < size; index++)
+{
+printf("Value checked array[%u] = [%d]\n", (unsigned int)index, array[index]);
+if (array[index] == value)
+{
+return (index);
+}
+}
+return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,16 +8,18 @@
  * @right: Ending index of the subarray to print
  */
 void print_array(int *array, size_t left, size_t right) {
-    
-	size_t i;
-	printf("Searching in array: ");
-    for ( i = left; i <= right; i++) {
-        printf("%d", array[i]);
-        if (i < right) {
-            printf(", ");
-        }
-    }
-    printf("\n");
+size_t i;
+
+printf("Searching in array: ");
+for ( i = left; i <= right; i++)
+{
+printf("%d", array[i]);
+if (i < right)
+{
+printf(", ");
+}
+}
+printf("\n");
 }
 
 /**
@@ -28,29 +30,33 @@ void print_array(int *array, size_t left, size_t right) {
  * 
  * Return: Index where value is located, or -1 if value is not present in array or if array is NULL
  */
-int binary_search(int *array, size_t size, int value) {
-	size_t mid;
-	size_t left =0;
-	size_t right = size - 1;
-    if (array == NULL) {
-        return -1;
-    }
-
-
-    while (left <= right) {
-        print_array(array, left, right);
-
-        mid = left + (right - left) / 2;
-
-        if (array[mid] == value) {
-            return mid;
-        } else if (array[mid] < value) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-
-    return -1;
+int binary_search(int *array, size_t size, int value)
+{
+size_t mid;
+size_t left =0;
+size_t right = size - 1;
+if (array == NULL)
+{
+return (-1);
 }
 
+while (left <= right)
+{
+print_array(array, left, right);
+mid = left + (right - left) / 2;
+
+if (array[mid] == value)
+{
+return mid;
+}
+else if (array[mid] < value)
+{
+left = mid + 1;
+}
+else
+{
+right = mid - 1;
+}
+}
+return (-1);
+}
